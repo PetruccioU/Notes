bfc073f3bbaac1bba86d717974d6d614befbe8a9
Petr Utkin <petr.utkin2022@gmail.com>
2024-11-27 17:26:48 +1300

    initial NotesController.cs created with mocking CRUD methods

---
+++ Notes/Controllers/NotesController.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;  // controller base import to inherit class NotesController
+
+namespace Notes.Controllers;
+
+[ApiController]  // attribute for this controller
+[Route("[controller]")] // route for this controller
+public class NotesController : ControllerBase
+{
+    [HttpGet]
+    public async Task<IActionResult> Get()
+    {
+        return Ok("Get");
+    }
+    
+    [HttpPost]
+    public async Task<IActionResult> Post()
+    {
+        return Ok("Post");
+    }
+
+    [HttpPut]
+    public async Task<IActionResult> Put()
+    {
+        return Ok("Put");
+    }
+
+    [HttpDelete]
+    public async Task<IActionResult> Delete()
+    {
+        return Ok("Delete");
+    }
+}
