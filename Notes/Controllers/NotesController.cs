428c3c484cdbd46dd8e1661bd4c2b2bf02be7d34
Petr Utkin <petr.utkin2022@gmail.com>
2024-12-01 13:07:58 +1300

    Get method expanded

--- Notes/Controllers/NotesController.cs
+++ Notes/Controllers/NotesController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Notes.Contracts;
 using Notes.DataAccess;
 using Notes.Models; // controller base import to inherit class NotesController
@@ -17,12 +18,12 @@ public class NotesController : ControllerBase
     }
     
     [HttpPost]
-    public async Task<IActionResult> Create([FromBody] CreateNotesContract request, CancellationToken ct) // put the creation method to external class "CreateNotesContract"
+    public async Task<IActionResult> Create([FromBody] CreateNotesRequestContract request, CancellationToken ct) // put the creation method to external class "CreateNotesContract"
     {
         var note = new NotesModel(request.Title, request.Description); 
         // obtain parameters from req and store into notes var 
         
-        await _dbContext.NotesModels.AddAsync(note, ct);  // add NotesModel object to db context
+        await _dbContext.Notes.AddAsync(note, ct);  // add NotesModel object to db context
         await _dbContext.SaveChangesAsync(ct);  // save changes to db 
         // ToDo: learn about CancellationToken (csrf protection?) 
         
@@ -30,10 +31,23 @@ public class NotesController : ControllerBase
     }
     
     [HttpGet]  // attribute for this method 
-    public async Task<IActionResult> Get()
+    public async Task<IActionResult> Get(GetNotesRequestContract request, CancellationToken ct)
     {
-        // ToDo: make pagination
+        var notesQuery = _dbContext.Notes
+            .Where(n => !string.IsNullOrEmpty(request.Search) && 
+                        n.Title.ToLower().Contains(request.Search.ToLower()));
+
+        if (request.Search == "desc")
+        {
+            notesQuery = notesQuery.OrderByDescending(n => n.CreatedAt);
+        }
+        else if (request.Search == "asc")
+        {
+            notesQuery = notesQuery.OrderBy(n => n.CreatedAt);
+        }
         
+        var notes = await notesQuery.ToListAsync(ct);
+        // ToDo: make pagination
         
         return Ok("Get");
     }
